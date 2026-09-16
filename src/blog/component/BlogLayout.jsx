1af233e00965760e3cdf53f3bbed86ae7819698c
Hiren Ray <166147435+hirenrayofficial@users.noreply.github.com>
2026-09-16 08:45:37 +0530

    Update BlogLayout.jsx

--- src/blog/component/BlogLayout.jsx
+++ src/blog/component/BlogLayout.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
 
 export default function BlogLayout() {
+  useEffect(() => {
+    // Create the script element
+    const script = document.createElement('script')
+    script.src = 'https://quge5.com/88/tag.min.js'
+    script.async = true
+    script.setAttribute('data-zone', '281237')
+    script.setAttribute('data-cfasync', 'false')
+
+    // Append the script to the document body (or head)
+    document.body.appendChild(script)
+
+    // Cleanup function to remove the script if the component unmounts
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
+
   return (
     <div>
       <header>
-        <Header/>
+        <Header />
       </header>
       <main>
-        <Outlet/>
+        <Outlet />
       </main>
       <footer>
-        <Footer/>
+        <Footer />
       </footer>
     </div>
   )
