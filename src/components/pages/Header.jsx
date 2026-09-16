a686ef926b4b8d3c3fd793a6a4c52154e1b4609a
Hiren Ray <166147435+hirenrayofficial@users.noreply.github.com>
2026-09-16 08:41:02 +0530

    Update Header.jsx

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
         transition={{ duration: 0.4 }}
         onClick={(e) => (window.location.href = "/")}
       >
-        <span>Rohit Roy</span>
+        <span>Hiren Ray</span>
       </motion.div>
       <motion.div
         className="center-header"
